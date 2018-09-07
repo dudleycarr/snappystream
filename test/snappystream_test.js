0a47a88618dd595961f9b9727faed7b70d847c6d
Dudley Carr <dudley.carr@gmail.com>
2018-09-07 16:42:32 -0700

    Use crc32c instead of crc32. Adjust bitshift operators.

--- test/snappystream_test.js
+++ test/snappystream_test.js
@@ -76,7 +76,7 @@ describe('SnappyStream', () => {
     })
 
     it('should have a valid checksum mask', () =>
-      compressedFrames.readUInt32LE(4).should.eql(0x70ed9df2))
+      compressedFrames.readUInt32LE(4).should.eql(0x63885e6b))
 
     return it('should have match decompressed data', done => {
       const payload = compressedFrames.slice(8)
